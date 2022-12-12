84a15a61275b294217380be7f1a4e4abf824d0ad
immalcolm <malcolm_yam@np.edu.sg>
2022-12-13 00:16:08 +0800

    simple api updates

---
+++ index.js
@@ -0,0 +1,74 @@
+// SETUP EXPRESS
+const express = require('express');
+const cors = require('cors');
+const mongodb = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const MongoClient = mongodb.MongoClient;
+const dotenv = require('dotenv');
+dotenv.config(); //remember to create .env file and setup based on your mongodb details
+
+let app = express();
+app.use(express.json());
+app.use(cors());
+
+// connect to the Mongo DB
+async function connect() {
+    const mongo_url = process.env.MONGO_URI;
+    let client = await MongoClient.connect(mongo_url, {
+        "useUnifiedTopology": true
+    })
+    //db to connect to 
+    let databaseName = "fake_recipes";
+    let db = client.db(databaseName);
+    console.log(`database connected: ${databaseName}`);
+    return db;
+}
+
+// ROUTES
+
+async function main() {
+    let db = await connect();
+
+    app.get('/recipes', async (req, res) => {
+        let recipes = await db.collection('recipes').find().toArray();
+        res.json(recipes)
+    })
+
+    app.get('/recipes/:recipeId', async (req, res) => {
+        let r = await db.collection('recipes').findOne({
+            _id: new ObjectId(req.params.recipeId)
+        });
+        res.json(r);
+    })
+
+    app.post('/recipes', async (req, res) => {
+        let results = await db.collection('recipes').insertOne({
+            title: req.body.title,
+            ingredients: req.body.ingredients
+        })
+        res.json(results);
+    })
+
+    app.patch('/recipes/:id', async (req, res) => {
+        let results = await db.collection('recipes').updateOne({
+            '_id': new ObjectId(req.params.id),
+        }, {
+            '$set': {
+                'title': req.body.title,
+                'ingredients': req.body.ingredients
+            }
+        })
+        res.json({
+            'status': true
+        })
+    })
+}
+
+
+main();
+
+// START SERVER
+// note: we set port to 8888 so it won't clash with React
+app.listen(8888, () => {
+    console.log("server has started")
+})
